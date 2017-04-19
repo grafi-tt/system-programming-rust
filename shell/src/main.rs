df67c11117e290ae7c119bccc9dbefa86520c69f
Shunsuke Shimizu <grafi@grafi.jp>
2017-04-19 14:28:10 +0900

    EOF

--- shell/src/main.rs
+++ shell/src/main.rs
@@ -22,14 +22,22 @@ fn main() {
 		let _ = stdout.write(PROMPT);
 		let _ = stdout.flush();
 		let mut line: Vec<u8> = vec![];
-		let _ = stdin_locked.read_until(b'\n', &mut line);
-		let pipeline = match parser::parse(&line) {
-			Ok(p) => p,
+		match stdin_locked.read_until(b'\n', &mut line) {
+			Ok(0) => { return; },
 			Err(e) => {
-				println!("parse error: {:?}", e);
-				continue;
+				use std::error::Error;
+				println!("read error: {:?}", e.description());
 			},
-		};
-		eval::eval(&mut state, &pipeline);
+			Ok(_) => {
+				let pipeline = match parser::parse(&line) {
+					Ok(p) => p,
+					Err(e) => {
+						println!("parse error: {:?}", e);
+						continue;
+					},
+				};
+				eval::eval(&mut state, &pipeline);
+			}
+		}
 	}
 }
