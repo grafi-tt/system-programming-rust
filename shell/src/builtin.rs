df793bfea9275a853cbf9c098f59f3d73f07924c
Shunsuke Shimizu <grafi@grafi.jp>
2017-04-19 19:47:42 +0900

    cd

--- shell/src/builtin.rs
+++ shell/src/builtin.rs
@@ -1,6 +1,24 @@
 use global;
 
-pub fn builtin_cd(_: &mut global::State, _: &Vec<&[u8]>) -> u8 {
+use std::env;
+use nix::unistd;
+
+const HOME_KEY: &'static str = "HOME";
+
+pub fn builtin_cd(_: &mut global::State, args: &Vec<&[u8]>) -> u8 {
+	if args.len() > 2 { return 1; }
+	let r = match args.get(0) {
+		Some(&dir) => unistd::chdir(dir),
+		None => match env::var_os(HOME_KEY) {
+			Some(ref dir) => unistd::chdir(dir.as_os_str()),
+			None => { return 2; },
+		}
+	};
+	if let Err(e) = r {
+		use std::error::Error;
+		println!("{}", e.description());
+		return 3;
+	}
 	0
 }
 
